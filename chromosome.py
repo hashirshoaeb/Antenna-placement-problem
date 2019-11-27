2c4e2473a3672ed8d7a6e5c18fc408bdf63577ca
hashirshoaeb <hashirshoaeb@gmail.com>
2019-11-27 17:27:16 +0500

    fitness value calculation function

--- chromosome.py
+++ chromosome.py
@@ -8,3 +8,24 @@ class chromosome:
         self.chrom = []
         for i in range(0,5):
                 self.chrom = self.chrom + [[r.choice(list),r.choice(list)]]
+        self.cal_fitness()
+        print(self.chrom, " with fitness ", self.fitnessValue)
+
+    def cal_fitness(self):
+        area = np.array([[0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0], ])
+        for i, j in self.chrom:
+            area[j - 1][i] = 1
+            area[j][i - 1] = area[j][i] = area[j][i + 1] = 1
+            area[j + 1][i] = 1
+        self.area = area[1:7, 1:7]
+        self.fitnessValue = self.area.sum()
+        # print(self.area)
+        # print(self.fitnessValue)
+        return self.fitnessValue
