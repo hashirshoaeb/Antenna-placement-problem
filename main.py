daa7de1e35072c04f2b561ddc8f6087805e08adf
hashirshoaeb <hashirshoaeb@gmail.com>
2019-11-27 17:31:57 +0500

    adding main file to test class

---
+++ main.py
@@ -0,0 +1,9 @@
+import random as r
+from chromosome import chromosome
+
+# main file example code:
+
+print ("///////   Generating list of 20 random chormosomes   ///////")
+list_of_20Rand = []
+for i in range(0, 20):
+    list_of_20Rand = list_of_20Rand + [chromosome()]
